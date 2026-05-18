43edec9d90c4df09cc602a484403c1c5c1d4625d
Chen Sakkol <chensakkol1124@gmail.com>
2026-05-18 18:20:36 +0700

    fix plant growth type enum

--- app/Modules/Inventory/Enums/PlantGrowthType.php
+++ app/Modules/Inventory/Enums/PlantGrowthType.php
@@ -6,7 +6,40 @@ namespace App\Modules\Inventory\Enums;
 
 enum PlantGrowthType: string
 {
-    case ANNUAL = 'annual';
-    case PERENNIAL = 'perennial';
-    case BIENNIAL = 'biennial';
+    case HERB = 'herb';
+    case SHRUB = 'shrub';
+    case TREE = 'tree';
+    case VINE = 'vine';
+    case GRASS = 'grass';
+    case AQUATIC = 'aquatic';
+    case OTHER = 'other';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::HERB => 'Herb',
+            self::SHRUB => 'Shrub',
+            self::TREE => 'Tree',
+            self::VINE => 'Vine',
+            self::GRASS => 'Grass',
+            self::AQUATIC => 'Aquatic',
+            self::OTHER => 'Other',
+        };
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function options(): array
+    {
+        return array_map(
+            fn(self $type) => [
+                'value' => $type->value,
+                'label' => $type->label(),
+            ],
+            self::cases()
+        );
+    }
 }
